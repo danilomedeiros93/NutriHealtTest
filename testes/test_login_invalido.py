e5cf77ceba35f0ebae67db733f3f8aed0a03288f
danilomedeiros93 <122568845+danilomedeiros93@users.noreply.github.com>
2023-11-19 21:22:31 -0300

    commit 1.0

---
+++ testes/test_login_invalido.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+class TestLogin:
+    
+    def setup_class(self):
+        global driver
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        
+    def test_login(self):
+        driver.get('http://localhost:3000/')  
+        
+
+        
+        time.sleep(10) 
+        
+        field_login = driver.find_element(By.CSS_SELECTOR, '#container_login > div.row > div.sc-hKgKIp.jITFtB.col-md-6 > div > input:nth-child(3)')
+        field_login.send_keys('danilo')
+        
+        field_password = driver.find_element(By.CSS_SELECTOR, '#container_login > div.row > div.sc-hKgKIp.jITFtB.col-md-6 > div > input:nth-child(4)')
+        field_password.send_keys('9999999')
+      
+        btn_link_sign_in = driver.find_element(By.CSS_SELECTOR,'#container_login > div.row > div.sc-hKgKIp.jITFtB.col-md-6 > div > div.sc-pGaPU.hXoUhX > button') 
+        
+        btn_link_sign_in.click()
+        
+       
+        
+        
+        
+        time.sleep(10) 
+        
+    def teardown_class(self):
+        driver.close()    
